48ee42cd8083deab75437dcfa6f68c9740886800
Tusharparihar <143849896+Tusharparihar@users.noreply.github.com>
2025-09-29 14:35:52 +0530

    Update version.tf

--- version.tf
+++ version.tf
@@ -3,10 +3,9 @@ terraform {
     azurerm = {
       source  = "hashicorp/azurerm"
       # This locks you into the v3.x.x series
-      version = ">= 3.0.0, < 4.46.1" 
+      version = ">= 3.0.0, < 4.46.0" 
     }
   }
-
   required_version = ">= 1.5.0, < 2.0.0"
 }
 }

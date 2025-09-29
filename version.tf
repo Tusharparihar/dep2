cd5c75bc6f1b61a9199e5645573784b0bcd26cc6
Tusharparihar <143849896+Tusharparihar@users.noreply.github.com>
2025-09-29 11:42:24 +0530

    Update version.tf

--- version.tf
+++ version.tf
@@ -1,7 +1,9 @@
 terraform {
-  required_version = ">= 1.5.0, < 2.0.0"
-}
-
-provider "azurerm" {
-  version = ">= 3.80.0, < 3.82.0"  # small range for test
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      # This constraint allows any version >= 2.0.0 but strictly < 3.0.0
+      version = ">= 2.0.0, < 3.0.0" 
+    }
+  }
 }

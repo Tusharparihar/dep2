f5bc43a1849c74ee93f0ed56c6979d8ddece8b4d
Tusharparihar <143849896+Tusharparihar@users.noreply.github.com>
2025-09-29 12:10:34 +0530

    Update version.tf

--- version.tf
+++ version.tf
@@ -1,10 +1,18 @@
-# versions.tf
+# This block specifies the required versions for Terraform and its providers.
+
 terraform {
+  required_version = "~> 1.0"
+  
   required_providers {
+    # We are explicitly locking the provider to the 4.35 minor series.
+    # The "~>" operator allows patch releases (e.g., 4.35.1, 4.35.2), 
+    # but prevents minor (4.36.0) or major (5.0.0) updates from being selected 
+    # during 'terraform init'. 
+    # However, Dependabot will detect new patch/minor versions that match 
+    # the constraint and generate PRs for them.
     azurerm = {
       source  = "hashicorp/azurerm"
-      # This locks you into the v3.x.x series
-      version = ">= 3.0.0, < 4.0.0" 
+      version = "~> 4.35" 
     }
   }
 }
